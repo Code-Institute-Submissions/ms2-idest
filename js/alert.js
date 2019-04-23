4b4019f04fd0f44ade0943a6b356b336245c8a47
Anthony Nicklin <anthonynicklin01@gmail.com>
2019-04-23 15:57:31 +0000

    Added addition close function for modal

--- js/alert.js
+++ js/alert.js
@@ -1,20 +1,26 @@
-//Listener
-//var submitForm = document.getElementById('submit-form').addEventListener('click', submitOk);
-
-//function submitOk () {
-   // alert("Thank you!\nWe look forward to dicussing your next city adventure.");
-    
-//}
-
 $(document).ready(function() {
 
+//Display modal when the submit botton on the booking form is clicked
 $('#submit-form').click(function() {
     $('.modal').css("display", "block");
 });
+
+//Hide the modal when the x is clicked
 $('.close-cross').click(function() {
   $('.modal').css("display", "none");
 });
+
+//Hide the modal when the close button is clicked
 $('#close').click(function() {
   $('.modal').css("display", "none");
 });
+
+//Close when the user clicks anywhere outside the modal
+var modal = document.getElementById('myModal');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 });
