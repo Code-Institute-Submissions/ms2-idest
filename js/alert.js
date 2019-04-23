f2bfd298c208255c4743a2e6783d98465add72a6
Anthony Nicklin <anthonynicklin01@gmail.com>
2019-04-23 13:16:39 +0000

    Inital JQuery now working for modal

--- js/alert.js
+++ js/alert.js
@@ -1,37 +1,20 @@
 //Listener
-var submitForm = document.getElementById('submit-form').addEventListener('click', submitOk);
+//var submitForm = document.getElementById('submit-form').addEventListener('click', submitOk);
 
-function submitOk () {
-    alert("Thank you!\nWe look forward to dicussing your next city adventure.");
+//function submitOk () {
+   // alert("Thank you!\nWe look forward to dicussing your next city adventure.");
     
-}
+//}
 
 $(document).ready(function() {
-    // Get the modal
-var modal = document.getElementById('myModal');
-$('.modal-btn').click(function() {
-    $('.submit-modal').css("display", "block");
+
+$('#submit-form').click(function() {
+    $('.modal').css("display", "block");
 });
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+$('.close-cross').click(function() {
+  $('.modal').css("display", "none");
+});
+$('#close').click(function() {
+  $('.modal').css("display", "none");
+});
 });
