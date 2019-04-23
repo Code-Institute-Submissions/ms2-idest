dd9c9afdf68256dd5627ecc62b568a2ce8556ab6
Anthony Nicklin <anthonynicklin01@gmail.com>
2019-04-23 21:01:32 +0000

    Created variables for  input validation for form

--- js/alert.js
+++ js/alert.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+  
+//Variables
+var valName = $('#name'),
+    valEmail = $('#email'),
+    valEmailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    valTelephone = $('#telephone'),
+    valTelephoneFormat = /^\d{10}$/;
+
+//Name input validation
+
 
 //Display modal when the submit botton on the booking form is clicked
-$('#submit-form').click(function() {
+$('#submit-form').click(function(event) {
+    event.preventDefault();
     $('.modal').css("display", "block");
 });
 
