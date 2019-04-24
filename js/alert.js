2a8b987d2abeed177f6d3470355d269e4b7f2e49
Anthony Nicklin <anthonynicklin01@gmail.com>
2019-04-24 09:44:56 +0000

    Reworked with emailjs. Modal for success submit

--- js/alert.js
+++ js/alert.js
@@ -1,37 +1,50 @@
-$(document).ready(function() {
-  
-//Variables
-var valName = $('#name'),
-    valEmail = $('#email'),
-    valEmailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    valTelephone = $('#telephone'),
-    valTelephoneFormat = /^\d{10}$/;
+//Function to sent email. If successful display modal with thank you message.
+//If the email fails console.log error.
 
-//Name input validation
-
-
-//Display modal when the submit botton on the booking form is clicked
-$('#submit-form').click(function(event) {
-    event.preventDefault();
-    $('.modal').css("display", "block");
-});
-
-//Hide the modal when the x is clicked
-$('.close-cross').click(function() {
-  $('.modal').css("display", "none");
-});
-
-//Hide the modal when the close button is clicked
-$('#close').click(function() {
-  $('.modal').css("display", "none");
-});
-
-//Close when the user clicks anywhere outside the modal
-var modal = document.getElementById('myModal');
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function sendEmail (formDetails) {
+    emailjs.send ("ant_cole909_gmail_com", "template_cN4Hm3Z3", {
+      "from_name": formDetails.fromName.value,
+      "from_email": formDetails.emailAddress.value,
+      "telephone": formDetails.telephone.value,
+      "poi": formDetails.poi.value,
+      "message": formDetails.message.value
+    })
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+      restForm();
+      var modal = document.getElementById('myModal');
+      var closeCross = document.getElementsByClassName("close-cross")[0];
+      var closeBtn  = document.getElementById('close');
+      
+      modal.style.display = "block";
+    
+      //Hide the modal when the x is clicked
+      closeCross.onclick = function() {
+        modal.style.display = "none";
+      };
+      //Hide the modal when the close button is clicked
+      closeBtn.onclick = function() {
+        modal.style.display = "none";
+      };
+      //Close when the user clicks anywhere outside the modal
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      };
+    
+    }, 
+    function (error) {
+        console.log('FAILED...', error);
+      }
+    );
+    return false;
   }
+
+function resetForm() {
+  document.getElementById("booking-form").reset();
 }
-});
+
+
+
+
