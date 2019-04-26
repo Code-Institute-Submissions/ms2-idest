d6c99d998d3a5efc73f0ef3f8c44b6b01f4eacdf
Anthony Nicklin <anthonynicklin01@gmail.com>
2019-04-26 14:39:09 +0000

    Function description grammar corrected

--- js/topPage.js
+++ js/topPage.js
@@ -1,6 +1,6 @@
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the button.
 window.onscroll = function() {
-    scrollFunction()
+    scrollFunction();
 };
 
 function scrollFunction () {
@@ -11,7 +11,7 @@ function scrollFunction () {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll to the top of the document.
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
