df03b6f117e4287cf76f743d14f94676e69aef65
Anthony Nicklin <anthonynicklin01@gmail.com>
2019-05-01 14:38:48 +0000

    Input validation for booking form

--- js/alert.js
+++ js/alert.js
@@ -1,16 +1,60 @@
-//Function to send email. If successful display modal with thank you message.
-//If the email fails console.log error.
-function sendEmail (formDetails) {
+//Form input validation. Takes the value from each input and ensures 
+//they are correct before submitting.
+function validateForm (){
+  
+    var name = document.forms["booking-form-form"]["fromName"].value;
+    var email = document.forms["booking-form-form"]["emailAddress"].value;
+    var phone = document.forms["booking-form-form"]["telephone"].value;
+    var poi = document.forms["booking-form-form"]["poi"].value;
+    var msg = document.forms["booking-form-form"]["message"].value;
+    
+    if (name == "") {
+        alert("Name must not be empty.");
+        return false;
+    }
+    if (name !== "") {
+        if (!(/^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/.test(name))) {
+            alert("Must only container characters.");
+            return false;
+        }
+    }
+     if (email == "") {
+        alert("Email must not be empty.")
+        return false;
+    }
+    if (email !== "") {
+        if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))) {
+            alert("Incorrect format. Must contain @domain.")
+            return false;
+        }
+    }
+    if (phone == "") {
+        alert("Telephone field must not be empty");
+        return false;
+    }
+    if (phone !== "") {
+        if (!(/^[0-9]*$/.test(phone))) {
+            alert("Must only contain numbers");
+            return false;
+        }
+    }
+    
+    //Once validation is complete emailjs is fired to send the details
+    //to an email address.
     emailjs.send ("ant_cole909_gmail_com", "template_cN4Hm3Z3", {
-      "from_name": formDetails.fromName.value,
-      "from_email": formDetails.emailAddress.value,
-      "telephone": formDetails.telephone.value,
-      "poi": formDetails.poi.value,
-      "message": formDetails.message.value
+      "from_name": name,
+      "from_email": email,
+      "telephone": phone.value,
+      "poi": poi,
+      "message": msg
     })
     .then(function(response) {
       console.log('SUCCESS!', response.status, response.text);
-      resetForm();
+     
+      document.getElementById("booking-form-form").reset();
+      
+      //If the email has been send with no errors then create and
+      //display modal with message.
       var modal = document.getElementById('myModal');
       var closeCross = document.getElementsByClassName("close-cross")[0];
       var closeBtn  = document.getElementById('close');
@@ -38,10 +82,6 @@ function sendEmail (formDetails) {
       }
     );
     return false;
-  }
-
-function resetForm() {
-  document.getElementById("booking-form").reset();
 }
 
 
